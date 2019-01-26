c490846c1266fb82072d4d4e7d00114c4dace538
DBadalzadehCSUN <Dvin.Badalzadeh.578@my.csun.edu>
2019-01-25 16:46:39 -0800

    Added Requirement.php and Time.php

--- app/Requirement.php
+++ app/Requirement.php
@@ -3,16 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-//use App\Schedule;
 
 class Requirement extends Model
 {
-    //
-   
-
-    public function schedule()
-    {
-        return $this->belongsTo('App\Schedule');
-    }
+    public function time()
+	{
+		return $this->hasOne('App\Time');
+	}
 }
- 
