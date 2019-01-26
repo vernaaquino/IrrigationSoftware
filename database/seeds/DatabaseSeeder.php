c8382b55241bd8934395755724a8afa491103657
Ghazal <ghazaljain334@gmail.com>
2019-01-25 17:16:53 -0800

    Schedule Seeder Stuff

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        
+       
+
+        $this->call(ScheduleSeeder::class);
+        //$this->call(IssueScheduleSeeder::class);
+
+       
+
     }
 }
