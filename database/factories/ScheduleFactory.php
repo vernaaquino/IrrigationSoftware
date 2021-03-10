ded79bd8df0a4e3621685feffac33fdfcf0eaa5f
Verna Aquino <verna.aquino09@gmail.com>
2021-03-09 19:02:53 -0800

    upgraded to laravel version 8.32.1

--- database/factories/ScheduleFactory.php
+++ database/factories/ScheduleFactory.php
@@ -1,16 +1,33 @@
 <?php
 
-use Faker\Generator as Faker;
+namespace Database\Factories;
 
-$factory->define(App\Schedule::class, function (Faker $faker) {
-    return [
-        //
-        //
-        //'id' => str_random(100),
-        'name' => $faker->name,
-        'num_of_days' => $faker->randomNumber($nbDigits = 2),
-        'anchor_day' => $faker->date($format = 'Y-m-d', $max = 'now'),
-        'weather_factor_id' => $faker->unique()->randomNumber($nbDigits = 4),
-        'season_factor_id' => $faker->unique()->randomNumber($nbDigits = 4),
-    ];
-});
+use App\Models\Schedule;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class ScheduleFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Schedule::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'num_of_days' => $faker->randomNumber($nbDigits = 2),
+            'anchor_day' => $faker->date($format = 'Y-m-d', $max = 'now'),
+            'weather_factor_id' => $faker->unique()->randomNumber($nbDigits = 4),
+            'season_factor_id' => $faker->unique()->randomNumber($nbDigits = 4),
+        ];
+    }
+}
